2ac1f5b4261ee867969bf46f1ba635195b397208
Ubuntu <ubuntu@ip-172-31-9-122.us-west-2.compute.internal>
2025-10-22 13:40:32 +0000

    update

--- cli-min/pre-requisites/eks-dev-instance-launch.sh
+++ cli-min/pre-requisites/eks-dev-instance-launch.sh
@@ -1,10 +1,12 @@
 
-name_tag=eks-hypd-workspace-$(date +"%m%d%H%M")
+# wget -O eks-hypd-workspace.yaml https://raw.githubusercontent.com/haozhx23/HyperPod-InstantStart/refs/heads/main/cli-min/pre-requisites/eks-hypd-workspace.yaml
+
+name_tag=ttttt-$(date +"%m%d%H%M")
 aws cloudformation create-stack \
   --stack-name $name_tag \
   --template-body file://eks-hypd-workspace.yaml \
   --parameters ParameterKey=ResourceTag,ParameterValue=$name_tag \
-              ParameterKey=InstanceType,ParameterValue=c5.2xlarge \
-              ParameterKey=EBSVolumeSize,ParameterValue=400 \
+              ParameterKey=InstanceType,ParameterValue=c5.xlarge \
+              ParameterKey=EBSVolumeSize,ParameterValue=200 \
               ParameterKey=KeyPairName,ParameterValue=pdxkeypair \
   --capabilities CAPABILITY_NAMED_IAM
